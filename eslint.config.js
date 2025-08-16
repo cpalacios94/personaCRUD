66ef8289e5d205efe7fc3538727d1ccf39fc3104
cpalacios94 <chrispalacios94@gmail.com>
2025-08-16 15:06:16 -0500

    all: cambios menores

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,36 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require('eslint/config')
+const expoConfig = require('eslint-config-expo/flat')
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
-  },
-]);
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    extends: [
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended'
+    ],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      },
+      'import/resolver': {
+        typescript: {}
+      }
+    },
+    rules: {
+      // Add any specific rules for JSX if needed
+    },
+    ignores: ['dist/*']
+  }
+])
